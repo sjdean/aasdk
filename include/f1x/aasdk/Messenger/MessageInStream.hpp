91eaa473eef22edc98d56c572253a3fdab831592
Simon Dean <sjdean@gmail.com>
2020-03-08 23:22:39 +0000

    Fix up variables

--- include/f1x/aasdk/Messenger/MessageInStream.hpp
+++ include/f1x/aasdk/Messenger/MessageInStream.hpp
@@ -41,9 +41,9 @@ public:
 private:
     using std::enable_shared_from_this<MessageInStream>::shared_from_this;
 
-    void receiveFrameHeaderHandler(const common::DataConstBuffer& buffer, int qid, int ism);
-    void receiveFrameSizeHandler(const common::DataConstBuffer& buffer, int qid, int ism);
-    void receiveFramePayloadHandler(const common::DataConstBuffer& buffer, int qid, int ism);
+    void receiveFrameHeaderHandler(const common::DataConstBuffer& buffer);
+    void receiveFrameSizeHandler(const common::DataConstBuffer& buffer);
+    void receiveFramePayloadHandler(const common::DataConstBuffer& buffer);
 
     boost::asio::io_service::strand strand_;
     transport::ITransport::Pointer transport_;
@@ -56,7 +56,7 @@ private:
     ChannelId channelId_;
     int calledFromFunction_;
     bool videoOnly_;
-    int qid);
+    int qid_;
     int ism_;
 };
 
